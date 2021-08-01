ede57605ba7076228ff935caef9de6e8b6b1d730
oje-edu <thorsten.oje@gfn.education>
2021-08-01 13:05:35 +0200

    added services

--- backend/services.py
+++ backend/services.py
@@ -3,3 +3,11 @@ import database as _database
 
 def create_database():
     return _database.Base.metadata.create_all(bind=_database.engine)
+
+
+def get_db():
+    db = _database.SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
